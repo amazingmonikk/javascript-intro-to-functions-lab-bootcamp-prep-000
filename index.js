b3022bcb32640d87cf0bf1e94918a2c8f8548609
Monika Tadross <monikk.tadross@gmail.com>
2019-02-02 02:04:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,9 +17,7 @@ function logWhisper(string) {
   return string.toLowerCase();
 }
 
-// var uppercase = "HELLO!"
-// var lowercase = "hello!"
-// var mixedCase = "I love you, Grandma"
+
  
 function sayHiToGrandma(string) {
   
