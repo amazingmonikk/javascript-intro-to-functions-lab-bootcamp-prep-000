26e5f70310a262e0d6efa14c0dff8a15feb9bfb1
Monika Tadross <monikk.tadross@gmail.com>
2019-02-02 01:18:21 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,43 @@
+function shout (string) {
+  return string.toUpperCase()
+}
+
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+ 
+function logShout(string) {
+  console.log('HELLO'); 
+  return string.toUpperCase()
+}
+
+function logWhisper(string) {
+  console.log('hello'); 
+  return string.toLowerCase()
+}
+
+ var uppercase = "HELLO!"
+ var lowercase = 'hello!'
+ var mixedCase = "I love you, Grandma"
+ 
+function sayHiToGrandma(string) {
+  
+  if ((string)===lowercase) {
+    return "I can't hear you!"  
+  }
+  
+  else if ((string)===uppercase) {
+    return "YES INDEED!"
+  }
+  
+  else ((string)==="I love you, Grandma."); {
+    return "I love you, too."
+  }
+  }
+
+
+
+
+
+
