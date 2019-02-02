ab5902c3f0695e668681634855e7387f85377f40
Monika Tadross <monikk.tadross@gmail.com>
2019-02-02 01:29:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,15 +23,15 @@ function logWhisper(string) {
  
 function sayHiToGrandma(string) {
   
-  if ((string)===lowercase) {
+  if (string === lowercase) {
     return "I can't hear you!"  
   }
   
-  else if ((string)===uppercase) {
+  else if (string === uppercase) {
     return "YES INDEED!"
   }
   
-  else ((string)==="I love you, Grandma."); {
+  else (string === "I love you, Grandma."); {
     return "I love you, too."
   }
   }
