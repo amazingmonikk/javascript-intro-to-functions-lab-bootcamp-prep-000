faa338052813b9ac07a7ae107bae43625d0feb77
Monika Tadross <monikk.tadross@gmail.com>
2019-02-02 02:11:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function sayHiToGrandma(string) {
     return "I can't hear you!";  
   }
   
-  else if (string === "HELLO!") {
+  else if (string === string.toUpperCase()) {
     return "YES INDEED!";
   }
   
